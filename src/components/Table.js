59c847409fe78926d01d038f046b4a0d6c3a6f63
jared Higgins <hjared10@yahoo.com>
2020-04-26 00:35:36 -0600

    added table

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,33 +1,66 @@
 import React, { Component } from "react";
-import API from '../utils/API';
+import axios from 'axios';
+
+
 
 
 class Table extends Component {
   state = {
-    results: []
-  };
+    persons: [],
+    persononen: '',
+    persononea: '',
+    persononel: '',
+    persontwon:'',
+    persontwoa: '',
+    persontwol: '',
+    personthreen: '',
+    personthreea: '',
+    personthreel: ''
+  }
 
   componentDidMount() {
-    this.findusers();
-  }
-
-  finderusers = () => {
-    API.getUsers()
+    axios.get("https:////randomuser.me/api/?results=10&nat=us")
     .then(res => {
-      const persons = res.data;
-      this.setState({persons})})
+      console.log(res.data.results)
+      const persons = res.data.results;
+      //console.log(persons[1])
+      this.setState({persons})
+      this.setState({persononen:persons[1].name.first})
+      this.setState({persononel:persons[1].name.last})
+      this.setState({persononea:persons[1].dob.age})
+      this.setState({persontwoa:persons[2].dob.age})
+      this.setState({persontwon:persons[2].name.first})
+      this.setState({persontwol:persons[2].name.last})
+      this.setState({personthreea:persons[3].dob.age})
+      this.setState({personthreen:persons[3].name.first})
+      this.setState({personthreel:persons[3].name.last})
+    })
   }
-
 
 render() {
   return (
-    <ul>
-      {this.state.persons.map(person => <li>{person.name}</li>)}
-    </ul>
+    <div>
+
+    <table style={{width:'100%'}}>
+  <tr>
+    <th>{this.state.persononen}</th>
+  <th>{this.state.persononel}</th>
+    <th>{this.state.persononea}</th>
+  </tr>
+  <tr>
+  <td>{this.state.persontwon}</td>
+  <td>{this.state.persontwol}</td>
+  <td>{this.state.persontwoa}</td>
+  </tr>
+  <tr>
+    <td>{this.state.personthreen}</td>
+    <td>{this.state.personthreel}</td>
+  <td>{this.state.personthreea}</td>
+  </tr>
+</table>
+    </div>
   )
 }
 }
 
 export default Table;
-
-//"https:////randomuser.me/api/?results=200&nat=us
